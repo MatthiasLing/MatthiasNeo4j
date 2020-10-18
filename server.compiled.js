18d477d58caae0d5d3f143fc05f6d97c0f7f29c8
matthiasling <matthiasling@uchicago.edu>
2020-10-18 13:39:25 -0500

    teehee

--- server.compiled.js
+++ server.compiled.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 5000;
 
 const neo4j = require('neo4j-driver');
 
