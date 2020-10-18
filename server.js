e97bafef07f879438c88a7dc1f057ecf2f29ce3d
matthiasling <matthiasling@uchicago.edu>
2020-10-18 04:32:53 -0500

    fsdfds

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const express = require('express');
 const app = express();
-const port = process.env.HTTP_PORT || 5000
+const port = process.env.port || 5000
 const neo4j = require('neo4j-driver');
 const bodyParser = require('body-parser');
 
